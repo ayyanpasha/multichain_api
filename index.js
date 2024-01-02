0fe61cd89b9f5d529a01d963cb10a8d5662de49c
ayyanpasha <ayyanpasha2014@gmail.com>
2024-01-02 11:33:18 +0530

    Add .gitignore file to ignore specified files/directories & Code Written

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+require("dotenv").config();
+
+let multichain = require("multichain-node")({
+    port: process.env.multichain_port,
+    host: process.env.multichain_host,
+    user: process.env.multichain_user,
+    pass: process.env.multichain_pass
+});
+
+
+Stream = "data"
+
+app.get('/', (req, res) => {
+    multichain.listStreamItems({ stream: Stream, count: 5 }, (err, result) => {
+        if (err) {
+            res.send("No Data")
+        } else {
+            var Data = "";
+            for (var i = 0; i < result.length - 1; i++) {
+                Data += JSON.stringify(result[i].data.json) + "<br>";  //Get Data from each entries
+            }
+            res.send(Data);
+        }
+    })
+});
+
+// Define a route
+app.get('/addData', (req, res) => {
+    let jsonData = { name: "testValue", city: "Mumbai" };
+    let hexData = new Buffer(JSON.stringify(jsonData)).toString("hex");
+
+    // Add JSON data to the stream
+    multichain.publish({
+        stream: Stream,
+        key: "key2",
+        data: hexData
+    }, (err, response) => {
+        if (err) {
+            res.send("Cannot add Data")
+        } else {
+            res.send("Created Data");
+        }
+    })
+
+});
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
